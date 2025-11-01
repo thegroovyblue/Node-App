87e1b1277ec0c59f18e7ca929a1179567a1bca8b
Joel Mitchell <joey5533a@gmail.com>
2025-11-01 04:47:53 -0500

    created index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+const app = express()
+app.use(bodyParser.json())
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const MONGOURI = process.env.MONGO_URI
+
+mongoose.connect(MONGOURI).then(() => {
+    console.log("Connected to MongoDB")
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch(error => console.log(error))
